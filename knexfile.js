1e1707707719e20c7d327718c681698fd883b97b
Jamieson Bates <jamiesonbates@gmail.com>
2017-05-19 10:07:10 -0700

    Configure knexfile

---
+++ knexfile.js
@@ -0,0 +1,32 @@
+module.exports = {
+  development: {
+    client: 'postgresql',
+    connection: 'postgres://localhost/socketChat_dev',
+    migrations: {
+      directory: __dirname + '/src/server/db/migrations'
+    },
+    seeds: {
+      directory: __dirname + '/src/server/db/seeds'
+    }
+  },
+  test: {
+    client: 'postgresql',
+    connection: 'postgres://localhost/socketChat_test',
+    migrations: {
+      directory: __dirname + '/src/server/db/migrations'
+    },
+    seeds: {
+      directory: __dirname + '/src/server/db/seeds'
+    }
+  },
+  production: {
+    client: 'postgresql',
+    connection: process.env.DATABASE_URL,
+    migrations: {
+      directory: __dirname + '/src/server/db/migrations'
+    },
+    seeds: {
+      directory: __dirname + '/src/server/db/seeds'
+    }
+  }
+}
