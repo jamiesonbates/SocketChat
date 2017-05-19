cfeb5be6117ca1c964c6103d59753d57f5771644
Jamieson Bates <jamiesonbates@gmail.com>
2017-05-19 10:33:47 -0700

    Add users_chats migration

---
+++ src/server/db/migrations/20170519103152_users_chats.js
@@ -0,0 +1,14 @@
+'use strict';
+
+exports.up = function(knex, Promise) {
+  return knex.schema.createTable('users_chats', (table) => {
+    table.increments();
+    table.integer('user_id').references('id').inTable('users');
+    table.integer('chat_id').references('id').inTable('chats');
+    table.timestamps(true, true);
+  });
+};
+
+exports.down = function(knex, Promise) {
+  return knex.schema.dropTable('users_chats');
+};
