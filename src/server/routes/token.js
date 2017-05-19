5cbb1609a92fcaaff65a67a333591be96f51b5eb
Jamieson Bates <jamiesonbates@gmail.com>
2017-05-19 15:10:12 -0700

    Add route for authenticating a user and creating a claim/token/cookie to send back

--- src/server/routes/token.js
+++ src/server/routes/token.js
@@ -3,7 +3,49 @@
 const bcrypt = require('bcrypt-as-promised');
 const boom = require('boom');
 const db = require('./db/connection');
+const { camelizeKeys, decamelizeKeys } = require('humps');
 const router = require('express').Router();
 const jwt = require('jsonwebtoken');
-
 const util = require('./util');
+
+router.post('/token', (req, res, next) => {
+  const { username, password } = req.body;
+  let user;
+
+  db('users')
+    .where('username', username)
+    .first()
+    .then((row) => {
+      if (!row) {
+        throw boom.create(400, 'Bad email or password.');
+      }
+
+      user = row;
+
+      return bcrypt.compare(password, user.h_pw)
+    })
+    .then(() => {
+      const claim = { userId: user.id };
+      const token = jwt.sign(claim, process.env.JWT_KEY, {
+        expiresIn: '30 days'
+      });
+
+      res.cookie('token', token, {
+        httpOnly: true,
+        expires: new Date(Date.now() + 1000 * 60 * 60 * 30),
+        secure: router.get('env') === 'production'
+      });
+
+      delete user.h_pw;
+
+      res.send(user);
+    })
+    .catch(bcrypt.MISMATCH_ERROR, () => {
+      throw boom.create(400, 'Bad email or password');
+    })
+    .catch((err) => {
+      next(err);
+    });
+});
+
+module.exports = router;
