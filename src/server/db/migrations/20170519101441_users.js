3b85f9a91cb4da78cf395734178e0ed3498e5d9f
Jamieson Bates <jamiesonbates@gmail.com>
2017-05-19 13:27:52 -0700

    Fix typos in migrations;

--- src/server/db/migrations/20170519101441_users.js
+++ src/server/db/migrations/20170519101441_users.js
@@ -2,7 +2,7 @@
 
 exports.up = function(knex, Promise) {
   return knex.schema.createTable('users', (table) => {
-    table.increment();
+    table.increments();
     table.string('first_name');
     table.string('last_name');
     table
