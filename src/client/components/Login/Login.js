2384f70d860292854508f7177a1e4cfdca68b0de
Jamieson Bates <jamiesonbates@gmail.com>
2017-05-23 14:15:41 -0700

    Migrate some code over from another project for signup and login

--- src/client/components/Login/Login.js
+++ src/client/components/Login/Login.js
@@ -5,11 +5,69 @@ import './Login.css';
 import Nav from '../Nav/Nav';
 
 class Login extends React.Component {
+  constructor() {
+    this.state = {
+      error: null
+    }
+  }
+
+  handleSubmit(e) {
+    e.preventDefault();
+
+    const email = this.refs.email.value;
+    const password = this.refs.password.value;
+
+    if (!email.includes('@')) {
+      this.setState({
+        error: 'Bad email or password.'
+      })
+
+      return;
+    }
+
+    if (password.length < 8) {
+      this.setState({
+        error: 'Bad email or password.'
+      })
+
+      return;
+    }
+
+    this.props.dispatch(userLogin(email, password));
+    this.refs.loginForm.reset();
+  }
+
   render() {
     return (
-      <div>
+      <div className="Login-container">
         <Nav />
-        <h2>Login</h2>
+
+        <div className="Login-form-container">
+          <form onSubmit={this.handleSubmit.bind(this)} ref="loginForm" className="Login-form">
+            <h2>Login</h2>
+            <div className="Login-input-container">
+              <input type="text" ref="email" placeholder="Email"/>
+            </div>
+
+            <div className="Login-input-container">
+              <input type="password" ref="password" placeholder="Password"/>
+            </div>
+
+            {
+              this.state.error || this.props.loginError ?
+                <div className="Login-error">
+                  <p>{this.state.error || this.props.loginError}</p>
+                </div>
+              : null
+            }
+
+            <button
+              type="submit"
+              className="Login-btn">
+              Login
+            </button>
+          </form>
+        </div>
       </div>
     )
   }
--- src/client/components/SignUp/SignUp.js
+++ src/client/components/SignUp/SignUp.js
@@ -5,11 +5,90 @@ import './SignUp.css';
 import Nav from '../Nav/Nav';
 
 class SignUp extends React.Component {
+  constructor() {
+    this.state = {
+      error: null
+    }
+  }
+
+  handleSubmit(e) {
+    e.preventDefault();
+
+    const username = this.refs.username.value;
+    const email = this.refs.email.value;
+    const password = this.refs.password.value;
+
+    if (username.length > 15) {
+      this.setState({
+        error: 'Username must be less than 15 characters.'
+      });
+
+      return;
+    }
+
+    if (password.length < 8) {
+      this.setState({
+        error: 'Password must be longer than 8 characters.'
+      });
+
+      return;
+    }
+
+    if (!email) {
+      this.setState({
+        error: 'Email must not be blank.'
+      })
+    }
+
+    if (!email.includes('@')) {
+      this.setState({
+        error: 'Must use valid email.'
+      })
+
+      return;
+    }
+
+    this.props.dispatch(userSignUp(username, email, password));
+    this.refs.signupForm.reset();
+  }
+
   render() {
     return (
-      <div>
+      <div className="SignUp-container">
         <Nav />
-        <h2>SignUp</h2>
+
+        <div className="Signup-form-container">
+          <h2>Sign Up</h2>
+
+          <form onSubmit={this.handleSubmit.bind(this)} ref="signupForm" className="Signup-form">
+            <div className="Signup-input-container">
+              <input type="text" ref="username" placeholder="Username"/>
+            </div>
+
+            <div className="Signup-input-container">
+              <input type="text" ref="email" placeholder="Email"/>
+            </div>
+
+            <div className="Signup-input-container">
+              <input type="password" ref="password" placeholder="Password"/>
+            </div>
+
+            {
+              this.state.error || this.props.signUpError ?
+                <div className="Signup-error">
+                  <p>{this.state.error || this.props.signUpError}</p>
+                </div>
+              :
+                null
+            }
+
+            <button
+              type="submit"
+              className="Signup-btn">
+              Start
+            </button>
+          </form>
+        </div>
       </div>
     )
   }
