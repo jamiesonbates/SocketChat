296d03669d5b0234ef9b4d72b16a9c8ae84c6bf6
Jamieson Bates <jamiesonbates@gmail.com>
2017-07-11 18:05:58 -0700

    Add basic structure to bookmarking activity

--- src/client/components/Dashboard/DefaultMain/BookmarksPre/BookmarksPre.js
+++ src/client/components/Dashboard/DefaultMain/BookmarksPre/BookmarksPre.js
@@ -1,16 +1,21 @@
 import React from 'react';
+import moment from 'moment';
 
 import './BookmarksPre.scss';
 
 const BookmarksPre = ({ recentBookmarks }) => {
+  console.log(recentBookmarks);
   return (
     <div className="DefaultMain-recent-container">
       <h3>Recent Bookmarks</h3>
       {
         recentBookmarks ?
           recentBookmarks.map((bookmark, i) => (
-            <div className="BookmarksPre-bookmark">
-              { bookmark.message }
+            <div key={i} className="BookmarksPre-bookmark">
+              <p>
+                {bookmark.first_name} {bookmark.last_name[0]}: {bookmark.message}
+              </p>
+              <p>{moment(bookmark).format('M/D/YY')}</p>
             </div>
           ))
         : null
