26696ca7d922e728d801bcdb3f1452f86fa8785f
Jamieson Bates <jamiesonbates@gmail.com>
2017-07-17 10:11:23 -0700

    Integrate newMessages store state into default main display

--- src/client/components/Dashboard/DefaultMain/ChatsPre/ChatsPre.js
+++ src/client/components/Dashboard/DefaultMain/ChatsPre/ChatsPre.js
@@ -2,9 +2,10 @@ import React from 'react';
 import moment from 'moment';
 import FaChat from 'react-icons/lib/ti/message';
 
+import Utilities from '../../../../utilities/Utilities';
 import './ChatsPre.scss';
 
-const ChatsPre = ({ chats, determineChatHeader, handleChatClick }) => {
+const ChatsPre = ({ chats, newMessages, determineChatHeader, handleChatClick }) => {
   return (
     <div className="DefaultMain-recent">
       <div className="DefaultMain-recent-header">
@@ -12,34 +13,40 @@ const ChatsPre = ({ chats, determineChatHeader, handleChatClick }) => {
       </div>
       {
         chats ?
-          chats.map((chat, i) => (
-            <div
-              className="ChatsPre-chat"
-              key={i}
-              onClick={() => handleChatClick(chat.id)}
-            >
-              <div className="ChatsPre-chat-top">
-                <div>
-                  <FaChat className="ChatsPre-chat-icon" />
-                  <p className="ChatsPre-chat-header">
-                  {determineChatHeader(chat)}
-                </p>
+          chats.map((chat, i) => {
+            const newMessagesChat = Utilities.findChat(newMessages, chat.id);
+            const count = newMessagesChat ? newMessagesChat.count : chat.count;
+
+            return (
+              <div
+                className="ChatsPre-chat"
+                key={i}
+                onClick={() => handleChatClick(chat.id)}
+              >
+                <div className="ChatsPre-chat-top">
+                  <div>
+                    <FaChat className="ChatsPre-chat-icon" />
+                    <p className="ChatsPre-chat-header">
+                    {determineChatHeader(chat)}
+                  </p>
+                  </div>
+                  <p className="ChatsPre-time">
+                    {moment(chat.lastActivity).fromNow(true)}
+                  </p>
                 </div>
-                <p className="ChatsPre-time">
-                  {moment(chat.lastActivity).fromNow(true)}
-                </p>
+                <div className="ChatsPre-chat-bottom">
+                  <p>{
+                      count > 0 ?
+                        count > 1 ?
+                          `${count} new messages`
+                        : `${count} new message`
+                      : `You're up to date`
+                    }
+                  </p>
+                </div>
               </div>
-              <div className="ChatsPre-chat-bottom">
-                <p>{chat.count > 0 ?
-                      chat.count > 1 ?
-                        `${chat.count} new messages`
-                      : `${chat.count} new message`
-                    : `You're up to date`
-                  }
-                </p>
-              </div>
-            </div>
-          ))
+            )
+          })
         : <div className="ChatsPre-chat">
             <p>You haven't started any chats yet.</p>
           </div>
