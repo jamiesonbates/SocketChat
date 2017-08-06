0fb420bcb6e2bfe06e17f7bd7087b926c329cf75
Jamieson Bates <jamiesonbates@gmail.com>
2017-08-06 12:23:32 -0700

    Implement photos instead of icons in appropriate places throughout app

--- src/client/utilities/userIcons.js
+++ src/client/utilities/userIcons.js
@@ -5,6 +5,7 @@ function userIconMaker(users, type) {
   switch(type) {
     case 'FOR_PROFILE':
       let url = users[0].cloudinaryUrl.split('/');
+
       url = [
         ...url.splice(0, url.indexOf('upload') + 1),
         'g_face,c_fill,r_max,h_300,w_300',
@@ -14,15 +15,26 @@ function userIconMaker(users, type) {
       return (
         <img
           src={url.join('/')}
-          className="UserProfile-profile-image"
+          className="Profile-image"
         />
       )
     case 'FOR_CHAT':
-      return (
-        <div className="UserIcon-chat icon">
+      let chatUrl;
+
+      if (users[0].cloudinaryUrl) {
+        chatUrl = users[0].cloudinaryUrl.split('/');
+        chatUrl = [
+          ...chatUrl.splice(0, chatUrl.indexOf('upload') + 1),
+          'g_face,c_fill,r_max,h_300,w_300',
+          ...chatUrl.splice(chatUrl.indexOf('upload') + 1)
+        ];
+      }
+
+      return users[0].cloudinaryUrl ?
+        <img src={chatUrl.join('/')} className="Chat-profile-image" />
+      : <div className="UserIcon-chat icon">
           <h3>{users[0].firstName[0]}</h3>
         </div>
-      )
     break;
 
     case 'FOR_CONTACT':
@@ -37,11 +49,23 @@ function userIconMaker(users, type) {
       let icon;
 
       if (users.length < 2) {
-        icon = (
-          <div className="UserIcon-side icon">
+        let url;
+
+        if (users[0].cloudinaryUrl) {
+          url = users[0].cloudinaryUrl.split('/');
+          url = [
+            ...url.splice(0, url.indexOf('upload') + 1),
+            'g_face,c_fill,r_max,h_300,w_300',
+            ...url.splice(url.indexOf('upload') + 1)
+          ];
+        }
+
+        icon = users[0].cloudinaryUrl ?
+          <img src={url.join('/')} className="Side-profile-image" />
+        : <div className="UserIcon-side icon">
             <h3>{users[0].firstName[0]}</h3>
           </div>
-        )
+
       }
       else if (users.length > 4) {
         icon = (
@@ -79,11 +103,30 @@ function userIconMaker(users, type) {
         icon = (
           <div className="UserIcon-standard-container icon">
             {
-              users.map((user, i) =>
-                <div key={i} className={i === 0 ? 'UserIcon-standard top' : 'UserIcon-standard bottom'}>
-                  <h3>{user.firstName[0]}</h3>
-                </div>
-              )
+              users.map((user, i) => {
+                let url;
+
+                if (user.cloudinaryUrl) {
+                  url = user.cloudinaryUrl.split('/');
+                  url = [
+                    ...url.splice(0, url.indexOf('upload') + 1),
+                    'g_face,c_fill,r_max,h_300,w_300',
+                    ...url.splice(url.indexOf('upload') + 1)
+                  ];
+                }
+
+                return user.cloudinaryUrl ?
+                  <img
+                    key={i}
+                    src={url.join('/')}
+                    className={ i === 0 ? "Side-profile-image top" : 'Side-profile-image bottom' }
+                  />
+                : <div
+                    key={i}
+                    className={i === 0 ? 'UserIcon-standard top' :'UserIcon-standard bottom'}>
+                    <h3>{user.firstName[0]}</h3>
+                  </div>
+              })
             }
           </div>
         )
