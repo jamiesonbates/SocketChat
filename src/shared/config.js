95b091c30d367a5acb709d136cc63a7c65a94068
Jamieson Bates <jamiesonbates@gmail.com>
2017-08-11 12:06:31 -0700

    Prepare the Heroku

--- src/shared/config.js
+++ src/shared/config.js
@@ -1,6 +1,6 @@
 export const WEB_PORT = process.env.PORT || 8000;
 export const STATIC_PATH = '/static';
-export const APP_NAME = 'Hello App';
+export const APP_NAME = 'Socket Chat';
 
 export const WDS_PORT = 7000;
 
